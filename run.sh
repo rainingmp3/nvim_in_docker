a9d56134fedbfc5be9b889681a6a749ab491b435
rainingmp3 <dotagenii@gmail.com>
2025-08-01 09:55:49 +0000

    Added volume link to home after coppying files to it via Docker cp

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker run -it --rm \
   --env="QT_X11_NO_MITSHM=1" \
   --env="LIBGL_ALWAYS_SOFTWARE=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="/home/green/Code/ros2/ssos_ws_future/ssos_ws/:/root/ssos_ws"\
   --volume="/home/green/.config/nvim/:/root/.config/nvim:rw" \
   --volume="/home/green/.local/share/nvim/:/root/.local/share/nvim:rw" \
   --volume="/home/green/.cache/nvim/:/root/.cache/nvim:rw" \
